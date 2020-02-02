f4e84871f5a181c0530836419b3fadf3fba5582e
gzy <593947521@qq.com>
2020-02-02 10:50:20 +0800

    2.2

---
+++ Code/75.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+    	l, r, i = 0, len(nums)-1, 0
+    	while i <= r:
+    		if nums[i] == 0:
+    			nums[i] = 1
+    			nums[l] = 0
+    			l += 1
+    		elif nums[i] == 2:
+    			temp = nums[r]
+    			nums[r] = 2
+    			nums[i] = temp
+    			r -= 1
+    			continue
+    		i += 1
+    	print(nums)
+
+Solution().sortColors([])
