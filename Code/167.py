52b838752e2aac69702a5abb1dcb76612c8ca253
gzy <593947521@qq.com>
2020-02-14 11:33:24 +0800

    2.14

--- Code/167.py
+++ Code/167.py
@@ -7,4 +7,14 @@ class Solution:
     		if ans.get(target - num) != None:
     			return [ans.get(target - num), i+1]
     		ans[num] = i + 1
-    
+
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    	l, r = 0, len(numbers) - 1
+    	while l < r:
+    		if numbers[l] + numbers[r] == target:
+    			return [l, r]
+    		elif numbers[l] + numbers[r] > target:
+    			r -= 1
+    		else:
+    			l += 1
