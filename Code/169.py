03bd5958ccfb6faba4b0e69b1b375f75c325cf2d
gzy <593947521@qq.com>
2020-02-15 21:03:23 +0800

    2.15

---
+++ Code/169.py
@@ -0,0 +1,19 @@
+
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+    	nums = sorted(nums)
+    	return nums[len(nums) // 2]
+
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
